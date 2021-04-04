e90c90768eed8299deb43135b786230846946e32
Stephen Akuffo <sa.akuffo@gmail.com>
2021-04-04 01:22:57 -0400

    refactor to functions

--- cash_growth/cash_growth.py
+++ cash_growth/cash_growth.py
@@ -1,4 +1,4 @@
-starting_amount = 10000
+import sys
 
 def grow_cash(st_amt, gw_pct, num_rota):
   computed_amount = st_amt
@@ -7,4 +7,15 @@ def grow_cash(st_amt, gw_pct, num_rota):
 
   return computed_amount
 
-print(grow_cash(starting_amount, 1.1, 1))
+def growth_predictions(args):
+  starting_amount = int(args[1])
+  growth_percentage = float(args[2])
+  num_of_rotations = int(args[3])
+
+  return grow_cash(starting_amount, growth_percentage, num_of_rotations)
+
+result = growth_predictions(sys.argv)
+print(result)
+
+
+
