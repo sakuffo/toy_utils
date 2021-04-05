217ba70fec1e847b3d4f795b0d6e1db86e62c608
Stephen Akuffo <sa.akuffo@gmail.com>
2021-04-05 00:03:23 -0400

    json file add in

--- cash_growth/cash_growth.py
+++ cash_growth/cash_growth.py
@@ -3,21 +3,25 @@ import sys
 import json
 
 # %%
-def grow_cash(st_amt, gw_pct, num_rota):
-  computed_amount = st_amt
-  return computed_amount * gw_pct**num_rota
+def grow_cash(json_data):
+  starting_amount = int(json_data["st_amt"])
+  growth_percentage = float(json_data["gw_pct"])
+  num_of_rotations = int(json_data["num_rota"])
+  return starting_amount * growth_percentage ** num_of_rotations
 
 # %%
 def growth_predictions(args):
-  starting_amount = int(args["st_amt"])
-  growth_percentage = float(args["gw_pct"])
-  num_of_rotations = int(args["num_rota"])
-
-  return grow_cash(starting_amount, growth_percentage, num_of_rotations)
+  if len(args) == 1:
+    return grow_cash(args[0]["st_amt"], args[0]["gw_pct"], args[0]["num_rota"])
+  else:
+    all_results= []
+    for x in args:
+      all_results.append(grow_cash(x))
+    return all_results
 
 # %%
 
-def load_file(json_file='./test.json'):
+def load_file(json_file):
   with open(json_file) as f:
     data=json.loads(f.read())
     f.close()
@@ -25,8 +29,10 @@ def load_file(json_file='./test.json'):
 
 # %%
 
-data = load_file()
+data = load_file(sys.argv[1])
 result = growth_predictions(data)
 print(result)
+
+# %%
 
 # %%
